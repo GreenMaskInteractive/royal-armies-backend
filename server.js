45d17fc7ac78447c68e870aa4cd48430a6e5a955
GreenMaskInteractive <greenmaskproductions@gmail.com>
2026-05-11 00:13:55 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@
 const express = require('express');
 const path = require('path');
 const compression = require('compression');
-const nodemailer = require('nodemailer');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
